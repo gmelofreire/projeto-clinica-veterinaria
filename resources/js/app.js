ff75383b0b24a5c9d40884b10f2524056f8bfde9
Gustavo Freire <gustavo.freire@hfa.mil.br>
2024-07-04 14:11:14 -0300

    #GF Criado componente AvaliacoesIndex

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,14 @@
-import './bootstrap';
 import { createApp } from 'vue';
 import Cabecalho from './components/estrutura/Cabecalho.vue';
 import Banner from './components/Banner.vue';
 import Exames from './components/Exames.vue';
+import AvaliacoesIndex from './components/AvaliacoesIndex.vue';
 
 const app = createApp();
 
 app.component('cabecalho', Cabecalho);
 app.component('banner', Banner);
 app.component('exames', Exames);
+app.component('avaliacoesindex', AvaliacoesIndex);
 
 app.mount('#app');
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('master')
 
 @section('content')
-    <Cabecalho> </Cabecalho>
+    <Cabecalho></Cabecalho>
     <banner image-path="../../img/banner-horizontal.jpg"></banner>
 
     <div class="bg-primaria mx-auto max-w-7xl px-32 py-11 mt-20 rounded-3xl">
@@ -16,7 +16,11 @@
 
     <div class="mt-20">
         <h3 class="text-[32px] text-center font-semibold text-black ">Avaliações</h3>
+        <avaliacoesindex :quant="6"></avaliacoesindex>
+    </div>
+
+    <div class="mt-20">
+        <h3 class="text-[32px] text-center font-semibold text-black ">Atendimento</h3>
         
     </div>
-    
 @endsection
